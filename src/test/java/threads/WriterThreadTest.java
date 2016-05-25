dbd0513a23b2a4c810769da06af075c889dea142
Thiago G. Alves <thiagogarciaalves@mail.com>
2016-05-25 00:07:43 -0300

    Testando Threads

---
+++ src/test/java/threads/WriterThreadTest.java
@@ -0,0 +1,29 @@
+package threads;
+
+import org.junit.Test;
+import threads.WriterThread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriterThreadTest {
+
+    @Test
+    public void WriterThreadTest01() throws InterruptedException {
+
+        List<WriterThread> threadList = new ArrayList<WriterThread>();
+
+        for (int i = 0; i < 100; i++) {
+            WriterThread writerThread = new WriterThread("Thread_"+i,100);
+            writerThread.start();
+            threadList.add(writerThread);
+        }
+
+        // Aguarda todas as threads morrerem
+        for (WriterThread writerThread : threadList) {
+            writerThread.join();
+        }
+
+    }
+
+}
