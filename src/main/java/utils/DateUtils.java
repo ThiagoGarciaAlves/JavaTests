4dc70b5e9d6597ceca52cbdaaf9d54fcae67191c
Thiago G. Alves <thiagogarciaalves@mail.com>
2016-06-08 22:36:35 -0300

    Concluindo o commit anterior (ops!)

--- src/main/java/utils/DateUtils.java
+++ src/main/java/utils/DateUtils.java
@@ -1,4 +1,19 @@
 package utils;
 
+import java.util.Calendar;
+
 public class DateUtils {
+
+    public static int millisecondsDay(Calendar date) {
+        int hours = date.get(Calendar.HOUR_OF_DAY);
+        int minutes = date.get(Calendar.MINUTE);
+        int seconds = date.get(Calendar.SECOND);
+        int milliseconds = date.get(Calendar.MILLISECOND);
+        int milliHours = hours*60*60*1000;
+        int milliMinutes = minutes*60*1000;
+        int milliSeconds = seconds*1000;
+        int result = milliHours + milliMinutes + milliSeconds + milliseconds;
+        return result;
+    }
+
 }
--- src/test/java/utils/DateUtilsTest.java
+++ src/test/java/utils/DateUtilsTest.java
@@ -1,4 +1,21 @@
 package utils;
 
+import org.junit.Test;
+
+import java.util.Calendar;
+
+import static org.junit.Assert.assertEquals;
+
 public class DateUtilsTest {
+
+    @Test
+    public void test001() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 32400000);
+        assertEquals(32400000, DateUtils.millisecondsDay(calendar));
+    }
+
 }
