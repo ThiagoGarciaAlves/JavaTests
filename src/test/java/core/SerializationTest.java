574b6901bc18b13bec268318c65b4b58e94f4486
Thiago G. Alves <thiago.alves@icolabora.com.br>
2018-03-01 15:45:03 -0300

    Customizando a serialização

--- src/test/java/core/SerializationTest.java
+++ src/test/java/core/SerializationTest.java
@@ -40,6 +40,33 @@ public class SerializationTest {
 	}
 
 	@Test
+	public void customSerializingDeserializing() throws IOException, ClassNotFoundException {
+		Person p = new Person();
+		p.setAge(20);
+		p.setName("Joe");
+		p.setHeight(175);
+
+		FileOutputStream fileOutputStream
+				= new FileOutputStream("person.txt");
+		ObjectOutputStream objectOutputStream
+				= new ObjectOutputStream(fileOutputStream);
+		objectOutputStream.writeObject(p);
+		objectOutputStream.flush();
+		objectOutputStream.close();
+
+		FileInputStream fileInputStream
+				= new FileInputStream("person.txt");
+		ObjectInputStream objectInputStream
+				= new ObjectInputStream(fileInputStream);
+		Person p2 = (Person) objectInputStream.readObject();
+		objectInputStream.close();
+
+		assertTrue(p2.getAge() == p.getAge());
+		assertTrue(p2.getName().equals(p.getName()));
+		assertTrue(p2.getHeight() == p.getHeight());
+	}
+
+	@Test
 	public void byteArrayCharsetTest() {
 		String sp = "São Paulo";
 		byte[] sp_bytes = sp.getBytes();
@@ -58,6 +85,18 @@ class Person implements Serializable {
 	private int age;
 	private String name;
 	private transient int height;
+
+	private void writeObject(ObjectOutputStream oos)
+			throws IOException {
+		oos.defaultWriteObject();
+		oos.writeInt(this.getHeight());
+	}
+
+	private void readObject(ObjectInputStream ois)
+			throws ClassNotFoundException, IOException {
+		ois.defaultReadObject();
+		this.setHeight(ois.readInt());
+	}
 
 	// getters and setters
 	public static String getCountry() {
